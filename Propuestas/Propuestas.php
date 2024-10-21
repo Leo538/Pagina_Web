c9647976d26b6d3c1ca82f7a43daad17fb8db1e7
Elkin <elkinjoao2004@hotmail.com>
2024-10-21 05:22:35 -0500

    Creación del archivo Propuestas.php con una estructura básica

---
+++ Propuestas/Propuestas.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Propuestas</title>
+    <link rel="stylesheet" href="../Home\Main\css\estilos.css">
+</head>
+<body>
+    <div id="content">
+        <h1>Propuestas</h1>
+        <p>Aquí puedes colocar todo el contenido de los propuestas de los candidatos.</p>
+    </div>
+</body>
+</html>
