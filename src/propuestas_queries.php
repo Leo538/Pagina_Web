c9c8ff27a81f7bae472d840843b2e2c440e7502a
Elkin <elkinjoao2004@hotmail.com>
2024-10-21 20:15:56 -0500

    Se creó el archivo propuestas_queries.php para la conexión con la base de datos, utilizando consultas preparadas

--- src/propuestas_queries.php
+++ src/propuestas_queries.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
 
-include('config.php');
+include('../config/config.php');
 
 $categoria = isset($_POST['category']) ? $_POST['category'] : 'all';
 
